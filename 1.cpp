d12a261434bb9274919cb212bf359d6ffcc6f9f8
sagar1952115 <sagarkumar1952115@gmail.com>
2022-08-05 23:31:00 +0530

    first commit

---
+++ 1.cpp
@@ -0,0 +1,4 @@
+This is a dummy file to start the repository.
+In this repository i will be adding some of the good and very important dp solutions solved by me.
+I hope this will give me some sort of motivation.
+Thanks.. :-)
